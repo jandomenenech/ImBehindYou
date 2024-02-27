7969eb6efe83e993586c882c956e931a6890890d
JanDomen <jandomenech@ceroca.cat>
2024-02-27 16:32:43 +0100

    Bala + Codigo Disparo

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public GameObject balaPrefab;
+    public Transform puntoDeDisparo;
+    public float fuerzaDeDisparo = 10f;
+    private float tiempo;
+
+
+    private void Start()
+    {
+
+    }
+    public void Disparar()
+    {
+        GameObject bala = Instantiate(balaPrefab, puntoDeDisparo.position, puntoDeDisparo.rotation);
+
+        Rigidbody balaRigidbody = bala.GetComponent<Rigidbody>();
+
+        if (balaRigidbody != null)
+        {
+           
+            balaRigidbody.AddForce(-puntoDeDisparo.forward * fuerzaDeDisparo, ForceMode.Impulse);
+        }
+        else
+        {
+            Debug.LogError("El prefab de la bala no tiene un componente Rigidbody.");
+        }
+    }
+}
