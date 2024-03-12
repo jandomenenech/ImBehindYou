66a42283cd6e4986af885d424d3af7d594ef80dd
jandomenech <jandomenech@ceroca.cat>
2024-03-12 13:37:00 +0100

    Animacion Disparo

---
+++ Assets/Scripts/CambioDeArmas.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioDeArmas : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> armas = new List<GameObject>();
+    [SerializeField] private Animator animator;
+    void Start()
+    {
+        foreach (GameObject arma in armas)
+        {
+            arma.SetActive(false);
+        }
+
+        if (armas.Count > 0)
+        {
+            armas[0].SetActive(true);
+            animator.SetBool("Rifle", false);
+            animator.SetBool("Pistol", true);
+        }
+    }
+
+    void Update()
+    {
+        cambiarArma();
+    }
+
+    public void cambiarArma()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivarArma(0);
+            animator.SetBool("Rifle",false);
+            animator.SetBool("Pistol",true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActivarArma(1);
+            animator.SetBool("Rifle", true);
+            animator.SetBool("Pistol", false);
+        }
+
+
+    }
+    void ActivarArma(int indice)
+    {
+        DesactivarTodasArmas();
+
+        if (indice >= 0 && indice < armas.Count)
+        {
+            armas[indice].SetActive(true);
+        }
+    }
+
+    void DesactivarTodasArmas()
+    {
+        foreach (GameObject arma in armas)
+        {
+            arma.SetActive(false);
+        }
+    }
+}
